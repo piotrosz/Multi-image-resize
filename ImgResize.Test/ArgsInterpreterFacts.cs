6c97773cd903db611e6c9b259dbb7fc4895b083a
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-11-26 15:27:57 +0100

    Changed folder structure

---
+++ ImgResize.Test/ArgsInterpreterFacts.cs
@@ -0,0 +1,92 @@
+﻿using Xunit;
+
+namespace ImgResize.Test
+{
+    public class ArgsInterpreterFacts
+    {
+        public class GetSettingsMethod
+        {
+            [Fact]
+            public void should_return_null_for_show_help_option()
+            {
+                // Arrange
+                var args = new[] {"--help"};
+
+                // Act
+                var result = ArgsInterpreter.GetSettings(args);
+
+                // Assert
+                Assert.Null(result);
+            }
+
+            [Fact]
+            public void should_set_width_error_for_invalid_width_argument()
+            {
+                // Arrange
+                var args = new[] { "-i", "test1", "-o", "test2", "-w", "-90" };
+
+                // Act
+                var result = ArgsInterpreter.GetSettings(args);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.True(result.WidthError);
+                Assert.Equal("test1", result.InputDir);
+                Assert.Equal("test2", result.OutputDir);
+            }
+
+            [Fact]
+            public void should_set_quality_error_for_invalid_quality()
+            {
+                // Arrange
+                var args = new[] { "-i", "test1", "-o", "test2", "-w", "90", "-q", "110" };
+
+                // Act
+                var result = ArgsInterpreter.GetSettings(args);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.True(result.QualityError);
+            }
+
+            [Fact]
+            public void should_set_font_size_error_for_invalid_font_size_argument()
+            {
+                // Arrange
+                var args = new[] { "-i", "test1", "-o", "test2", "-w", "90", "-fs", "asc" };
+
+                // Act
+                var result = ArgsInterpreter.GetSettings(args);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.True(result.FontSizeError);
+                Assert.Equal("test1", result.InputDir);
+                Assert.Equal("test2", result.OutputDir);
+            }
+
+            [Fact]
+            public void should_return_settings_for_minimal_options()
+            {
+                // Arrange
+                var args = new[] { "-i", "test1", "-o", "test2", "-w", "190" };
+
+                // Act
+                var result = ArgsInterpreter.GetSettings(args);
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal(190, result.Width);
+                Assert.Equal(false, result.WidthError);
+                Assert.Equal("test1", result.InputDir);
+                Assert.Equal("test2", result.OutputDir);
+                Assert.False(result.FontSizeError);
+                Assert.False(result.HeightError);
+                Assert.False(result.QualityError);
+                Assert.False(result.WidthError);
+                Assert.False(result.ShowHelp);
+                Assert.False(result.Verbose);
+            }
+        }
+    }
+}
---
+++ ImgResize.Test/SettingsValidatorFacts.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace ImgResize.Test
+{
+    public class SettingsValidatorFacts
+    {
+        public class ValidateMethod
+        {
+            [Fact]
+            public void validation_should_fail_for_no_options()
+            {
+                // Arrange
+                var settings = new Settings();
+                var target = new SettingsValidator(settings);
+
+                // Act
+                var result = target.Validate();
+
+                // Assert
+                Assert.NotNull(result.ErrorMessages);
+                Assert.Equal(3, result.ErrorMessages.Count);
+            }
+
+            [Fact]
+            public void validation_should_pass_for_minimal_arguments()
+            {
+                // Arrange
+                var settings = new Settings
+                    {
+                        InputDir = "C:\\",
+                        OutputDir = "C:\\",
+                        Width = 120
+                    };
+                var target = new SettingsValidator(settings);
+
+                // Act
+                var result = target.Validate();
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal(0, result.ErrorMessages.Count);
+            }
+        }
+    }
+}
